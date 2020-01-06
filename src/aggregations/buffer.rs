foniod/foniod

commit 53a432fce731fcb16fdc212500b15561df4f7cc3

Make sure Buffer can be flushed when maximum number of elements reached

src/aggregations/buffer.rs
@@ -50,6 +50,10 @@ impl Aggregator {
         }
     }
 
+    pub fn max_len(&self) -> usize {
+        *[self.counters.len(), self.gauges.len(), self.timers.len(), self.sets.len(), self.histograms.len()].iter().max().unwrap()
+    }
+
     pub fn record<T: Into<Measurement>>(&mut self, measurement: T) {
         use kind::*;
 
@@ -264,6 +268,12 @@ impl Handler<Message> for Buffer {
     type Result = ();
 
     fn handle(&mut self, msg: Message, _ctx: &mut Context<Self>) -> Self::Result {
+        if let Some(max_elems) = self.config.max_records {
+            if max_elems > self.aggregator.max_len() as u64 {
+                self.aggregator.flush();
+            }
+        }
+
         match msg {
             Message::List(mut ms) => {
                 for m in ms.drain(..) {
@@ -288,6 +298,7 @@ pub struct BufferConfig {
     #[serde(default = "default_interval_ms")]
     pub interval_ms: u64,
     pub interval_s: Option<u64>,
+    pub max_records: Option<u64>,
     #[serde(default = "default_enable_histograms")]
     pub enable_histograms: bool,
 }

alessandrod/ingraind

commit 0113b4fccedef41772d4c0adc3facc412723316d

buffer: don't discard metrics when max_records is reached
This fixes a bug introduced in 53a432fce731fcb16fdc212500b15561df4f7cc3,
where metrics were accidentally being dropped instead of being flushed
upstream.

src/aggregations/buffer.rs
@@ -282,7 +282,7 @@ impl Handler<Message> for Buffer {
     fn handle(&mut self, msg: Message, ctx: &mut Context<Self>) -> Self::Result {
         if let Some(max_elems) = self.config.max_records {
             if self.aggregator.max_len() > max_elems as usize {
-                self.aggregator.flush();
+                self.flush(ctx);
             }
         }
 